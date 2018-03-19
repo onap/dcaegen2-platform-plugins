91c82edf114553ac3a4d441f6f1ccf18e398b31c
Lusheng Ji <lji@research.att.com>
2018-03-19 12:08:03 -0400

    Add pushing wagon as archive
    This is the fix needed for using mvn-phase-lib new feature as submitted
    at https://gerrit.onap.org/r/#/c/36395/, uploading Cloudify Manager plugins
    as zip and tgz files.
    Also this change cleans up the mvn-phase-script.sh by removing code that is
    redendunt (already in mvn-script-lib).
    
    Issue-ID: DCAEGEN2-206
    Change-Id: I922dd10f5423e19535d6f24506585b398028a2eb
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ================================================================================
-# Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2017-2018 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============LICENSE_END=========================================================
-#
-# ECOMP is a trademark and service mark of AT&T Intellectual Property.
 
 set -ex
 
@@ -26,67 +24,13 @@ echo "running script: [$0] for module [$1] at stage [$2]"
 MVN_PROJECT_MODULEID="$1"
 MVN_PHASE="$2"
 
-
 PROJECT_ROOT=$(dirname $0)
-
-FQDN="${MVN_PROJECT_GROUPID}.${MVN_PROJECT_ARTIFACTID}"
-if [ "$MVN_PROJECT_MODULEID" == "__" ]; then
-  MVN_PROJECT_MODULEID=""
-fi
-
-if [[ "$MVN_PROJECT_VERSION" == *SNAPSHOT ]]; then
-  echo "=> for SNAPSHOT artifact build"
-  MVN_DEPLOYMENT_TYPE='SNAPSHOT'
-else
-  echo "=> for STAGING/RELEASE artifact build"
-  MVN_DEPLOYMENT_TYPE='STAGING'
-fi
-echo "MVN_DEPLOYMENT_TYPE is             [$MVN_DEPLOYMENT_TYPE]"
-
-
-TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
-
-# expected environment variables
-if [ -z "${MVN_NEXUSPROXY}" ]; then
-    echo "MVN_NEXUSPROXY environment variable not set.  Cannot proceed"
-    exit
-fi
-MVN_NEXUSPROXY_HOST=$(echo "$MVN_NEXUSPROXY" |cut -f3 -d'/' | cut -f1 -d':')
-echo "=> Nexus Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
-
-if [ -z "$WORKSPACE" ]; then
-    WORKSPACE=$(pwd)
-fi
-
-export SETTINGS_FILE=${SETTINGS_FILE:-$HOME/.m2/settings.xml}
-   
-
-
-# mvn phase in life cycle
-MVN_PHASE="$2"
-
-
-echo "MVN_PROJECT_MODULEID is            [$MVN_PROJECT_MODULEID]"
-echo "MVN_PHASE is                       [$MVN_PHASE]"
-echo "MVN_PROJECT_GROUPID is             [$MVN_PROJECT_GROUPID]"
-echo "MVN_PROJECT_ARTIFACTID is          [$MVN_PROJECT_ARTIFACTID]"
-echo "MVN_PROJECT_VERSION is             [$MVN_PROJECT_VERSION]"
-echo "MVN_NEXUSPROXY is                  [$MVN_NEXUSPROXY]"
-echo "MVN_RAWREPO_BASEURL_UPLOAD is      [$MVN_RAWREPO_BASEURL_UPLOAD]"
-echo "MVN_RAWREPO_BASEURL_DOWNLOAD is    [$MVN_RAWREPO_BASEURL_DOWNLOAD]"
-MVN_RAWREPO_HOST=$(echo "$MVN_RAWREPO_BASEURL_UPLOAD" | cut -f3 -d'/' |cut -f1 -d':')
-echo "MVN_RAWREPO_HOST is                [$MVN_RAWREPO_HOST]"
-echo "MVN_RAWREPO_SERVERID is            [$MVN_RAWREPO_SERVERID]"
-echo "MVN_DOCKERREGISTRY_SNAPSHOT is     [$MVN_DOCKERREGISTRY_SNAPSHOT]"
-echo "MVN_DOCKERREGISTRY_RELEASE is      [$MVN_DOCKERREGISTRY_RELEASE]"
-echo "MVN_DOCKERREGISTRY_PUBLIC is       [$MVN_DOCKERREGISTRY_PUBLIC]"
 
 set -e
 if ! wget -O ${PROJECT_ROOT}/mvn-phase-lib.sh \
   "$MVN_RAWREPO_BASEURL_DOWNLOAD"/org.onap.dcaegen2.utils/R2/scripts/mvn-phase-lib.sh; then
   cp "${PROJECT_ROOT}"/scripts/mvn-phase-lib.sh "${PROJECT_ROOT}/mvn-phase-lib.sh"
 fi
-
 source "${PROJECT_ROOT}"/mvn-phase-lib.sh
 
 
@@ -112,6 +56,7 @@ test)
 package)
   echo "==> package phase script"
   build_wagons
+  build_archives_for_wagons
   ;;
 install)
   echo "==> install phase script"
@@ -119,6 +64,7 @@ install)
 deploy)
   echo "==> deploy phase script"
   upload_wagons_and_type_yamls
+  upload_wagon_archives
   ;;
 *)
   echo "==> unprocessed phase"
