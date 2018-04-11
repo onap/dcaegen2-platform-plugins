dfb29d3827efc341fb91afae4d96699d20085722
Alex Shatov <alexs@att.com>
2018-04-11 17:48:35 -0400

    2.3.0 dcaepolicyplugin and data types
    - discovering consul
      -- starts with /opt/onap/config.txt file in cloudify manager
          sample data for that file
    [consul]
    address=consul-server:8500
      -- if not found or not able to connect, trying consul:8500
      -- if not able to connect, trying localhost:8500
    
    - added and refactored unit tests for discovery -- coverage 79%
    
    Change-Id: I3712fe36df47d6117206d61ca4916c4dbc42d035
    Signed-off-by: Alex Shatov <alexs@att.com>
    Issue-ID: DCAEGEN2-438

--- dcae-policy/dcaepolicyplugin/discovery.py
+++ dcae-policy/dcaepolicyplugin/discovery.py
@@ -20,52 +20,112 @@
 
 import base64
 import json
+from ConfigParser import ConfigParser
 
 import requests
 from cloudify import ctx
 
-# it is safe to assume that consul agent is at localhost:8500 along with cloudify manager
-CONSUL_SERVICE_URL = "http://localhost:8500/v1/catalog/service/{0}"
-CONSUL_KV_MASK = "http://localhost:8500/v1/kv/{0}"
 
+class DiscoveryClient(object):
+    """
+    talking to consul either at
+    url from the config file /opt/onap/config.txt on Cloudify Manager host
+    or at http://consul:8500 or at http://localhost:8500.
+    """
+    CONFIG_PATH = "/opt/onap/config.txt"
+    SERVICE_MASK = "http://{consul_host}/v1/catalog/service/{key}"
+    KV_MASK = "http://{consul_host}/v1/kv/{key}"
 
-def discover_service_url(service_name):
-    """find the service record in consul"""
-    service_url = CONSUL_SERVICE_URL.format(service_name)
-    ctx.logger.info("getting service_url at {0}".format(service_url))
+    _lazy_inited = False
+    _consul_hosts = None
 
-    response = requests.get(service_url)
+    @staticmethod
+    def _lazy_init():
+        """find out where is consul - either from config file or hardcoded 'consul'"""
+        if DiscoveryClient._lazy_inited:
+            return
+        DiscoveryClient._lazy_inited = True
+        DiscoveryClient._consul_hosts = ["consul:8500", "localhost:8500"]
 
-    ctx.logger.info("got {0} for service_url at {1} response: {2}"
-                    .format(response.status_code, service_url, response.text))
+        try:
+            config_parser = ConfigParser()
+            if not config_parser.read(DiscoveryClient.CONFIG_PATH):
+                ctx.logger.warn("not found config file at {config_path}"
+                                .format(config_path=DiscoveryClient.CONFIG_PATH))
+                return
+            consul_host = config_parser.get('consul', 'address')
+            if not consul_host:
+                ctx.logger.warn("not found consul address in config file {config_path}"
+                                .format(config_path=DiscoveryClient.CONFIG_PATH))
+                return
+            DiscoveryClient._consul_hosts.insert(0, consul_host)
+            ctx.logger.info("got consul_host: {consul_host} from config file {config_path}"
+                            .format(consul_host=DiscoveryClient._consul_hosts[0],
+                                    config_path=DiscoveryClient.CONFIG_PATH))
 
-    if response.status_code != requests.codes.ok:
-        return
+        except Exception as ex:
+            ctx.logger.warn("failed to get consul host from file {config_path}: {ex}"
+                            .format(config_path=DiscoveryClient.CONFIG_PATH, ex=str(ex)))
 
-    resp_json = response.json()
-    if resp_json:
+    @staticmethod
+    def _safe_get_url(url_mask, key):
+        """safely http get to url"""
+        for consul_host in DiscoveryClient._consul_hosts:
+            try:
+                url = url_mask.format(consul_host=consul_host, key=key)
+                ctx.logger.info("get {0}".format(url))
+                return url, requests.get(url)
+
+            except requests.ConnectionError as ex:
+                ctx.logger.warn("ConnectionError - failed to get {0}: {1}".format(url, str(ex)))
+        return None, None
+
+    @staticmethod
+    def get_service_url(service_name):
+        """find the service record in consul"""
+        DiscoveryClient._lazy_init()
+
+        url, response = DiscoveryClient._safe_get_url(DiscoveryClient.SERVICE_MASK, service_name)
+
+        if not response:
+            ctx.logger.error("failed to get service_url for {0}".format(service_name))
+            return None
+
+        ctx.logger.info("got {0} for service_url at {1} response: {2}"
+                        .format(response.status_code, url, response.text))
+
+        if response.status_code != requests.codes.ok:
+            return None
+
+        resp_json = response.json()
+        if not resp_json:
+            return None
         service = resp_json[0]
         return "http://{0}:{1}".format(service["ServiceAddress"], service["ServicePort"])
 
 
-def discover_value(key):
-    """get the value for the key from consul-kv"""
-    kv_url = CONSUL_KV_MASK.format(key)
-    ctx.logger.info("getting kv at {0}".format(kv_url))
+    @staticmethod
+    def get_value(key):
+        """get the value for the key from consul-kv"""
+        DiscoveryClient._lazy_init()
 
-    response = requests.get(kv_url)
+        url, response = DiscoveryClient._safe_get_url(DiscoveryClient.KV_MASK, key)
 
-    ctx.logger.info("got {0} for kv at {1} response: {2}"
-                    .format(response.status_code, kv_url, response.text))
+        if not response:
+            ctx.logger.error("failed to get kv for {0}".format(key))
+            return None
 
-    if response.status_code != requests.codes.ok:
-        return
+        ctx.logger.info("got {0} for kv at {1} response: {2}"
+                        .format(response.status_code, url, response.text))
 
-    data = response.json()
-    if not data:
-        ctx.logger.error("failed discover_value %s", key)
-        return
-    value = base64.b64decode(data[0]["Value"]).decode("utf-8")
-    ctx.logger.info("consul-kv key=%s value(%s) data=%s",
-                    key, value, json.dumps(data))
-    return json.loads(value)
+        if response.status_code != requests.codes.ok:
+            return None
+
+        data = response.json()
+        if not data:
+            ctx.logger.error("failed to get kv for %s", key)
+            return None
+        value = base64.b64decode(data[0]["Value"]).decode("utf-8")
+        ctx.logger.info("consul-kv key=%s value(%s) data=%s",
+                        key, value, json.dumps(data))
+        return json.loads(value)
