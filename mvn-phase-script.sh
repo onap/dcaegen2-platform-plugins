719a5c0ce0a49cb156993fb0aeabfb3dd49b50a6
Lusheng Ji <lji@research.att.com>
2018-07-16 22:56:35 -0400

    Update release tag
    Issue-ID: DCAEGEN2-499
    Change-Id: I1c28ccd0b5e3322347a6ea00c756086848bff9d6
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -27,9 +27,16 @@ MVN_PHASE="$2"
 PROJECT_ROOT=$(dirname $0)
 
 set -e
+RELEASE_TAG=${MVN_RELEASE_TAG:-R3}
+if [ "$RELEASE_TAG" != "R1" ]; then
+  RELEASE_TAGGED_DIR="${RELEASE_TAG}/"
+else
+  RELEASE_TAGGED_DIR="releases"
+fi
 if ! wget -O ${PROJECT_ROOT}/mvn-phase-lib.sh \
-  "$MVN_RAWREPO_BASEURL_DOWNLOAD"/org.onap.dcaegen2.utils/R2/scripts/mvn-phase-lib.sh; then
-  cp "${PROJECT_ROOT}"/scripts/mvn-phase-lib.sh "${PROJECT_ROOT}/mvn-phase-lib.sh"
+  "$MVN_RAWREPO_BASEURL_DOWNLOAD"/org.onap.dcaegen2.utils/${RELEASE_TAGGED_DIR}scripts/mvn-phase-lib.sh; then
+  echo "Fail to download mvn-phase-lib.sh"
+  exit 1
 fi
 source "${PROJECT_ROOT}"/mvn-phase-lib.sh
 
