2ad1b8910f6971c5761d904cb2648d6369c046e9
Hansen, Tony (th1395) <th1395@att.com>
2020-05-13 18:55:54 +0000

    copy dmaap, helm, pgaas and sshkeyshare plugins from ccsdk to dcaegen2
    Change-Id: Ib257495de6c275c45f0c87a4b42ac21a2fab7979
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>
    Issue-ID: DCAEGEN2-2207
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>

--- k8s/k8splugin/cloudify_importer.py
+++ k8s/k8splugin/cloudify_importer.py
@@ -1,6 +1,7 @@
 # #######
 # Copyright (c) 2017 GigaSpaces Technologies Ltd. All rights reserved
 # Copyright (c) 2019 Pantheon.tech. All rights reserved
+# Copyright (c) 2020 AT&T Intellectual Property. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
