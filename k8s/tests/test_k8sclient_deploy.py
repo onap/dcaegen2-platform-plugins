c21e3145f4f9847c28a6dd4985cc49c7c5014948
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-06-26 16:53:53 +0200

    Add external tls container
    Issue-ID: DCAEGEN2-2252
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>
    Change-Id: I90cb79120ffaf634fc1f5b8a03a83abb30deb2b7

--- k8s/tests/test_k8sclient_deploy.py
+++ k8s/tests/test_k8sclient_deploy.py
@@ -2,6 +2,7 @@
 # org.onap.dcae
 # ================================================================================
 # Copyright (c) 2018-2020 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2020 Nokia. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,6 +23,7 @@
 
 import pytest
 from common import do_deploy
+from common import do_deploy_ext
 
 def test_deploy_full_tls(mockk8sapi):
     ''' Deploy component with a full TLS configuration, to act as a server '''
@@ -43,6 +45,14 @@ def test_deploy_no_tls_info(mockk8sapi):
     ''' TLS client only, but with cert directory configured '''
 
     dep, deployment_description = do_deploy()
+
+    app_container = dep.spec.template.spec.containers[0]
+    assert app_container.volume_mounts[2].mount_path == "/opt/dcae/cacert"
+
+def test_deploy_external_cert(mockk8sapi):
+    ''' Deploy component with external TLS configuration '''
+
+    dep, deployment_description = do_deploy_ext({"external_cert_directory": "/path/to/container/cert/directory", "use_external_tls": True, "cert_type": "P12", "ca_name": "myname", "external_certificate_parameters": {"common_name": "mycommonname", "sans": "mysans"}})
 
     app_container = dep.spec.template.spec.containers[0]
     assert app_container.volume_mounts[2].mount_path == "/opt/dcae/cacert"
