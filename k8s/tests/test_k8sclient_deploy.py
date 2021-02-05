ee3d33430411fe9a70cad2ee98afe128b92b1e08
Edyta Krukowska <edyta.krukowska@nokia.com>
2021-02-05 09:01:44 +0100

    Fix conditions for volumes
    Issue-ID: DCAEGEN2-2546
    Signed-off-by: Edyta Krukowska <edyta.krukowska@nokia.com>
    Change-Id: I62bb6a9bc6ed2efefafa491331ad1c5c477a924b

--- k8s/tests/test_k8sclient_deploy.py
+++ k8s/tests/test_k8sclient_deploy.py
@@ -2,7 +2,7 @@
 # org.onap.dcae
 # ================================================================================
 # Copyright (c) 2018-2020 AT&T Intellectual Property. All rights reserved.
-# Copyright (c) 2020 Nokia. All rights reserved.
+# Copyright (c) 2020-2021 Nokia. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
